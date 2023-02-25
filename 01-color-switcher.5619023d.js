f387422ea88b1f012e51c71efff5911e1ce1b809
olena-savchenko <112640422+olena-savchenko@users.noreply.github.com>
2023-02-25 16:08:45 +0000

    Deploying to gh-pages from @ olena-savchenko/goit-js-hw-09@ac5b17992c4d8374f891bf2fca615864fe640de7 🚀

---
+++ 01-color-switcher.5619023d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("body"),n=document.querySelector("button[data-stop]");t.classList.add("btn"),n.classList.add("btn");let o=null;function a(){e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){t.disabled=!0,o=setInterval(a,1e3)})),n.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.5619023d.js.map
