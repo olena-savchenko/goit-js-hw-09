b2bc7964c8928198be7445f84c3db719351e5093
Olena Savchenko <elena.volobuieva@gmail.com>
2023-03-01 11:37:43 +0300

    update part#1 task#9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -15,7 +15,6 @@ function getRandomHexColor() {
 }
   
 const btnStartRef = document.querySelector('button[data-start]');
-const bodyRef = document.querySelector('body');
 const btnStopRef = document.querySelector('button[data-stop]');
 
 // добавляє класс для стилізації кнопок (не по т/з)
@@ -23,24 +22,30 @@ btnStartRef.classList.add('btn');
 btnStopRef.classList.add('btn');
 
 let intervalId = null;
+btnStopRef.disabled = true;
 
 btnStartRef.addEventListener('click', onBtnStartClick);
 btnStopRef.addEventListener('click', onBtnStopClick);
 
 
 function onBtnStartClick() { 
-
+	bodyColorized();
+	
 	btnStartRef.disabled = true;
+	btnStopRef.disabled = false;
+	
 	intervalId = setInterval(bodyColorized, 1000);
 
 }
 function onBtnStopClick() { 
 	clearInterval(intervalId);
+	
 	btnStartRef.disabled = false;
+	btnStopRef.disabled = true;
 }
 
 function bodyColorized() {
-	bodyRef.style.backgroundColor = getRandomHexColor();
+	document.body.style.backgroundColor = getRandomHexColor();
 }
  
 
